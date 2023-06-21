81c2807760315e1013a462e2f175a2b21bcc9152
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2023-06-21 14:07:39 +0530

    texttyped remove + caret div

--- src/components/TextTyped.svelte
+++ src/components/TextTyped.svelte
@@ -1,24 +1,57 @@
 <script>
+	export let text
+	let input
+	let inner_html
+
+	function checkText() {
+		// Get caret position
+		// let c = window.getSelection().getRangeAt(0).startOffset
+		// console.log(c)
+		// console.log("checking...")
+		// if (input == text.slice(0, input.length)) {
+		// 	console.log("correct")
+		// } else {
+		// let inputt = input
+		// inner_html = ""
+		// for (let i = 0; i < inputt.length; i++) {
+		// 	if (inputt[i] == text[i]) {
+		// 		inner_html +=
+		// 			'<span class="text-textColor">' + text[i] + "</span>"
+		// 	} else {
+		// 		inner_html +=
+		// 			'<span class="text-errorColor">' + text[i] + "</span>"
+		// 	}
+		// }
+		// inner_html =
+		// 	'<span class="text-textColor">' +
+		// 	text.slice(0, input.length - 1) +
+		// 	"</span>" +
+		// 	'<span class="text-errorColor">' +
+		// 	text[input.length - 1] +
+		// 	"</span>" +
+		// 	'<span class="text-textColor">'
+	}
+	// }
 </script>
 
-<section>
+<section class="w-[100vw]">
 	<!-- container -->
-	<div
-		class="container mx-auto lg:w-[90%] pt-8 overflow-x-hidden overflow-y-auto"
-	>
+	<div class="container mx-auto lg:w-[90%] pt-3 overflow-hidden">
 		<div
 			contenteditable="true"
-			class="text text-2xl tracking-wider h-32 text-textColor caret-yellowAccent outline-none w-100"
-		>
-			There are two main categories of monkeys, (i) new world monkeys, and
-			(ii) old world monkeys. The new world monkeys are found in the
-			regions of South America and North
-		</div>
+			class="text text-2xl tracking-wider h-48 text-textColor caret-yellowAccent outline-none w-100"
+			bind:textContent={input}
+			bind:innerHTML={inner_html}
+			on:input={checkText}
+		/>
 	</div>
 </section>
 
 <style>
 	.text {
 		font-family: "Roboto Mono", monospace;
+	}
+	.error {
+		color: #ca4754;
 	}
 </style>
