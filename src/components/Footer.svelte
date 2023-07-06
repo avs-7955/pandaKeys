8b30f6dfac2e5dce2c0edb3317b51150719c9ec0
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2023-07-06 17:20:26 +0530

    footer commit

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,33 +4,27 @@
 		<div class="flex text-mainlight justify-around">
 			<a
 				href="https://github.com/avs-7955/monkeytype-clone"
-				class="hidden md:flex hover:text-textColor"
+				class=" hover:text-textColor"
 				><i class="fa-brands fa-github mt-1 mr-1" /> github</a
 			>
-			<a
+			<!-- <a
 				href="https://github.com/avs-7955/monkeytype-clone"
 				class="flex md:hidden hover:text-textColor"
 				><i class="fa-brands fa-github" /></a
-			>
-			<p>
-				Made using <span
-					><img
-						alt="SvleteKit logo"
-						src="https://raw.githubusercontent.com/gilbarbara/logos/master/logos/svelte-kit.svg"
-						class="w-24 inline"
-					/></span
-				>
+			> -->
+			<p class="hidden md:flex">
+				"Master the art of words with lightning speed!"
 			</p>
 			<a
 				href="mailto: ayushi.v.sharan@gmail.com"
-				class="hidden md:flex hover:text-textColor"
+				class="hover:text-textColor"
 				><i class="fa-solid fa-envelope mt-1 mr-1" /> Connect with me!</a
 			>
-			<a
+			<!-- <a
 				href="mailto: ayushi.v.sharan@gmail.com"
 				class="flex md:hidden hover:text-textColor"
 				><i class="fa-solid fa-envelope mt-1 mr-1" /></a
-			>
+			> -->
 		</div>
 	</div>
 </footer>
